8c155c6c903f159400eecde7d7b7dd849ff227c8
Venkat varma <kvenkatvarma@gmail.com>
2025-02-18 14:50:39 +0530

    something Exciting

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 const Counter =()=>{
    const [counterState,setCounterState] =  useState(()=>{
-      return {counter:10,title:"Fun"};
+      return {counter:10};
    });
+
+   const [titleState,setTitleState] =useState("Fun")
 
    function incrementCounter(){
     setCounterState((prevstate)=>{
@@ -19,7 +21,7 @@ const Counter =()=>{
 
     return(
         <div className="col-12 col-md-4 offset-md-4 border  text-white">
-            <span className="h2 pt-4 m-2 text-white-50">{counterState.title} Counter </span>
+            <span className="h2 pt-4 m-2 text-white-50">{titleState} Counter </span>
             <br />
             <button className="btn btn-success m-1" onClick={incrementCounter}>+1</button>
             <button className="btn btn-danger m-1" onClick={decrementCounter}>-1</button>
